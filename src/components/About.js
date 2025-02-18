de535e56651d6f38776ecf0de307f8dc7fbebda7
MuhammadAman16 <108739339+MuhammadAman16@users.noreply.github.com>
2025-02-18 17:36:29 +0500

    Add About.js

---
+++ src/components/About.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import './About.css';
+
+function About() {
+  return (
+    <section id="about" className="About">
+      <h2>About Me</h2>
+      <p>Welcome to my portfolio! I am a Full Stack Developer with experience in building modern web applications.</p>
+    </section>
+  );
+}
+
+export default About;
