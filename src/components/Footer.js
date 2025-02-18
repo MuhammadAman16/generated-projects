9ad5a926ab8ac016f25dc15278aad38aca0e438e
MuhammadAman16 <108739339+MuhammadAman16@users.noreply.github.com>
2025-02-18 17:36:40 +0500

    Add Footer.js

---
+++ src/components/Footer.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './Footer.css';
+
+function Footer() {
+  return (
+    <footer className="Footer">
+      <p>&copy; 2023 Full Stack Developer. All rights reserved.</p>
+    </footer>
+  );
+}
+
+export default Footer;
